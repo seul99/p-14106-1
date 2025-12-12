7e7c796db76d29038c261ff7bd1d84252cf34b49
sia <piao516@naver.com>
2025-12-12 11:17:40 +0900

    15

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [post, setPosts] = useState<{
+    id: number;
+    title: string;
+    content: string;
+  } | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts/1")
+      .then((res) => res.json())
+      .then(setPosts);
+  }, []);
+
+  return (
+    <>
+      <h1>글 상세페이지</h1>
+      {post == null && <div>로딩중...</div>}
+
+      {post !== null && (
+        <>
+          <div>번호 : {post?.id}</div>
+          <div>제목 : {post?.title}</div>
+          <div style={{ whiteSpace: "pre-line" }}>{post?.content}</div>
+        </>
+      )}
+    </>
+  );
+}
