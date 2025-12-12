470dc4ef6074eee7ff480b28ed7f49edb25aaed6
sia <piao516@naver.com>
2025-12-12 11:27:40 +0900

    찐 17

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Page() {
   // const posts = [
@@ -35,7 +36,9 @@ export default function Page() {
 
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
         ))}
       </ul>
     </>
